ae4814789ecff2c752bf2f9327067034e770b9f3
Blake McIntyre <blake@chartmogul.com>
2019-09-08 21:30:11 -0400

    Remove users.password_digest
    New version of devise doesn't use this.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_155353) do
+ActiveRecord::Schema.define(version: 2019_09_09_012410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 2019_07_20_155353) do
   create_table "users", id: :serial, force: :cascade do |t|
     t.string "email"
     t.string "name", null: false
-    t.string "password_digest"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
