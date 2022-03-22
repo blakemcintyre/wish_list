81c3244a58061b91d821e5a9bcc760693be26db0
Blake McIntyre <blake@flywheelsoftware.com>
2022-03-21 22:00:58 -0400

    Test for clearing empty notes

--- spec/models/item_claim_spec.rb
+++ spec/models/item_claim_spec.rb
@@ -1,4 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe ItemClaim, type: :model do
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+  let(:item) { create(:item, user: user1) }
+
+  describe '#clear_blanks via before_save' do
+    subject(:instance) { build(:item_claim, item: item, user: user2, notes: notes) }
+
+    before do
+      instance.save
+      instance.reload
+    end
+
+    context 'when notes is empty' do
+      let(:notes) { '' }
+
+      it 'sets it to nil' do
+        expect(instance.notes).to be_nil
+      end
+    end
+
+    context 'when notes has a value' do
+      let(:notes) { 'a value' }
+
+      it 'keeps it as is' do
+        expect(instance.notes).to eq(notes)
+      end
+    end
+  end
 end
