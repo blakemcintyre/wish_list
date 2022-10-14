8f6142962e4ca77167dc36bea6ae193b12bbc139
Blake McIntyre <blake@flywheelsoftware.com>
2022-10-14 12:55:43 -0400

    Redirect message for heroku
    Disabled all routes, give redirect message.

---
+++ app/controllers/redirect_controller.rb
@@ -0,0 +1,5 @@
+class RedirectController < ApplicationController
+  def index
+    render layout: false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
 
-  resources :lists, except: [:show] do
-    get ":user_id" => "lists#index", on: :collection
+  # resources :lists, except: [:show] do
+  #   get ":user_id" => "lists#index", on: :collection
 
-    patch :remove_claimed
-  end
+  #   patch :remove_claimed
+  # end
 
-  resources :item_claims, except: [:index, :show]
-  resources :categories, only: [:create, :new, :update, :destroy]
-  resources :users
+  # resources :item_claims, except: [:index, :show]
+  # resources :categories, only: [:create, :new, :update, :destroy]
+  # resources :users
 
-  root "lists#edit"
+  get '*path', to: 'redirect#index'
+
+  root "redirect#index"
 end
