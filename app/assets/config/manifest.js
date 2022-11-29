5a9532f50c668ad1f46d6f61544c4637172b4118
Blake McIntyre <blake@flywheelsoftware.com>
2022-11-28 22:34:02 -0500

    Proper SVG as image, not partial
    Add quick link to edit list on side bar when permission exists.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 //= link application.css
 //= link application.js
+
+//= link icons/arrows_left_right.svg
+//= link icons/infinity.svg
+//= link icons/pencil.svg
+//= link icons/trash.svg
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,10 @@ class ApplicationController < ActionController::Base
   private
 
   def set_side_bar_lists
-    @side_bar_lists ||= List.where
-                            .not(id: ListPermission.select(:list_id).where(user: current_user, claimable: false))
+    @side_bar_lists ||= List.select("lists.*, SUM(1) FILTER (WHERE list_permissions.user_id = #{current_user.id}) AS current_user_edit")
+                            .left_joins(:permissions)
+                            .where.not(id: ListPermission.select(:list_id).where(user: current_user, claimable: false))
+                            .group(:id)
                             .order(:name)
   end
 end
